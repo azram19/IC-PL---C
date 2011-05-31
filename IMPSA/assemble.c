2d766de1b74a736f435de889c5722e1f7b86d895
Lukasz Seweryn <moa.1991@gmail.com>
2011-05-31 21:30:44 +0100

    assembler

--- IMPSA/assemble.c
+++ IMPSA/assemble.c
@@ -13,7 +13,30 @@ struct map_node{
 char *outputPath;
 
 
+struct command{
+	//R (1) type function.
+	//0-5 opcode  | 6 - 10 R1 | 11 - 15 R2 | 16 - 20 R3 | unused |
 
+	//I (2) type function.
+	//0-5 opcode  | 6 - 10 R1 | 11 - 15 R2 | 16 - 31 Immediate value
+
+	//J (3) type function.
+	//0-5 opcode  | 6 - 31 Address
+	int type;
+
+	char label[16];
+	int opcode;
+
+	int r1;
+	int r3;
+	int r2;
+
+	int constantValue;
+	char labelValue[];
+};
+
+
+//❤        L S  .  .  . f  r  o  m         h   e  r   e       ❤
 int main(int argc, char *argv[]){
 	if(argc!=2){
 		printf("usage: %s filename", argv[0]);
@@ -27,8 +50,8 @@ int main(int argc, char *argv[]){
 				return 1;
 			}
 			else{
-				outputPath=argv[2];
-
+			 	outputPath=argv[2];
+//❤        L S  .  .  . e   n   d                              ❤
 				chuj();
 
 
