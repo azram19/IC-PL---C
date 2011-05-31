11f12070a20a1c8e96d99a53315b66c5d31f4b8f
Lukasz Koprowski <lk1510@fusion32.doc.ic.ac.uk>
2011-05-31 15:21:22 +0100

    Added my function definitions and fixed minor mistake.

--- IMPSE/emulate.c
+++ IMPSE/emulate.c
@@ -3,7 +3,7 @@
 #include<stdio.h>
 
 #define NUMBER_OF_REGISTERS 32
-#define SIE_OF_MEMORY 65536 
+#define SIZE_OF_MEMORY 65536 
 
 struct IMPSS{
     int registers[NUMBER_OF_REGISTERS];
@@ -11,8 +11,24 @@ struct IMPSS{
     int PC;
 };
 
+int bge(struct IMPSS* state, int body){
+
+    return 1;
+}
+int jmp(struct IMPSS* state, int body){
+
+    return 1;
+}
+int jr(struct IMPSS* state, int body){
+
+    return 1;
+}
+int jal(struct IMPSS* state, int body){
+
+    return 1;
+}
 
 int main(int argc, char *argv[]){
 
-	return 0;//HEhe
+	return 0;
 }
