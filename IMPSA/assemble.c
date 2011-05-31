deac1d3cc8c35f2c81655220e9a246e465db8c11
Lukasz Koprowski <lk1510@fusion32.doc.ic.ac.uk>
2011-05-31 20:42:09 +0100

    Save

--- IMPSA/assemble.c
+++ IMPSA/assemble.c
@@ -2,6 +2,13 @@
 #include<stdio.h>
 #include<string.h>
 
+struct map_node{
+    char * key;
+    int value;
+    map_node * left;
+    map_node * right;
+};
+
 
 int main(int argc, char *argv[]){
 	return 0;
