8037cbbd56b2e6dfc11ac48eec6f092ff122f2c0
Agnieszka <agnieszka.m.szefer@gmail.com>
2011-05-31 16:10:43 +0100

    Finished function subi.

--- IMPSE/emulate.c
+++ IMPSE/emulate.c
@@ -86,7 +86,20 @@ int addi(struct IMPSS* state, int address){
 }
 
 int subi(struct IMPSS* state, int address){
+	int mask = 0x03E00000;
+	int r1 = address & mask;
+	r1 = r1 >> 21;
 	
+	mask = 0x001F0000;
+	int r2 = address & mask;
+	r2 = r2 >> 16;
+
+	mask = 0x0000FFFF;
+	int immediate = address & mask;
+
+	state -> registers[r1] = state -> registers[r2] - immediate;
+	state -> PC += 4;
+	return 1;
 }
 
 int main(int argc, char *argv[]){
