24f78f5762857d025524d865e3a54994f6f541e5
Agnieszka <agnieszka.m.szefer@gmail.com>
2011-05-31 15:14:03 +0100

    Starting addi, first try to use commit and push

--- IMPSE/emulate.c
+++ IMPSE/emulate.c
@@ -1,6 +1,7 @@
 #include<string.h>
 #include<stdlib.h>
 #include<stdio.h>
+#include<stdint.h>
 
 #define NUMBER_OF_REGISTERS 32
 #define SIE_OF_MEMORY 65536 
@@ -11,6 +12,10 @@ struct IMPSS{
     int PC;
 };
 
+
+int addi(*IMPSS impssptr, int16_t immediate){
+	
+}
 
 int main(int argc, char *argv[]){
 
