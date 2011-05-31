104b6aacf73d9c4c6f0299556667cfe6bbc529d0
Łukasz Koprowski <azram19@gmail.com>
2011-05-31 14:53:40 +0100

    Added IMPSS struct.

--- IMPSE/emulate.c
+++ IMPSE/emulate.c
@@ -2,6 +2,16 @@
 #include<stdlib.h>
 #include<stdio.h>
 
+#define NUMBER_OF_REGISTERS 32
+#define SIE_OF_MEMORY 65536 
+
+struct IMPSS{
+    int registers[NUMBER_OF_REGISTERS];
+    char memory[SIZE_OF_MEMORY]; //I use `char` to get one byte memory cells
+    int PC;
+};
+
+
 int main(int argc, char *argv[]){
 
 	return 0;
