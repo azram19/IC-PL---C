aba2a46b0312a342e0b7e450f11974535f6362e4
Lukasz Koprowski <azram19@gmail.com>
2011-05-31 22:51:42 +0100

    Tree rotation done. BR tree almost implemented.

--- IMPSA/assemble.c
+++ IMPSA/assemble.c
@@ -11,6 +11,7 @@
 #define HALT 0
 #define ERROR -1
 
+
 struct map_node{
     char * key;
     int value;
@@ -20,6 +21,12 @@ struct map_node{
     struct map_node * parent;
 };
 
+struct map_map{
+    struct map_node * root;
+};
+
+
+struct map_node * op_codes_tree = NULL;
 
 int map_put(struct map_node * root, char * key, int value){
     struct map_node node;
@@ -59,15 +66,86 @@ int tree_insert(struct map_node * root, char * key, struct map_node * node){
     return SUCCESS;
 }
 
+void tree_rotate_right(struct map_map * map){
+    struct map_node * root = map -> root
+    map -> root = map -> root -> left;
+    root -> left = map -> root -> right;
+    map -> root -> right = root; 
+}
+
+void tree_rotate_left(struct map_map * map){
+    struct map_node * root = map -> root;
+    map -> root = map -> root -> right;
+    root -> right = map -> root -> left;
+    map -> root -> left = root;
+}
+
 int rb_insert(struct map_node * root, char * key, struct map_node * node){
     //node -> colour = red;
-    
+    rb_insert_case1(struct map_node * node)
     return SUCCESS;
+}
+
+int rb_insert_case1(struct map_node * node){
+    if(node -> parent == NULL){
+        node -> colour = 1
+    } else {
+        rb_insert_case2(node);
+    }
+}
+int rb_insert_case2(struct map_node * node){
+    if(node -> parent -> color == 1){
+        return;
+    } else {
+        rb_insert_case3(node);
+    }
+}
+int rb_insert_case3(struct map_node * node){
+    struct map_node * u = uncle(node), * g;
+    
+    if( u != NULL && u -> colour == 2){
+        node -> parent -> colour = 1;
+        u -> colour = 1;
+        g = grandparent(node);
+        g -> colour = 2;
+    } else {
+        insert_case4(node);
+    }
+}
+int rb_insert_case4(struct map_node * node){
+    struct map_node * g = grandparent;
+    struct map_map m;
+    struct map_map * m_ptr = &m;
+    m.root = node -> parent;
+    //TODO
+}
+int rb_insert_case5(struct map_node * node){
+
+}
+
+struct map_node * grandparent(struct map_node * node){
+    if(node != NULL && node -> parent != NULL){
+        return node -> parent -> parent;
+    } else {
+        return NULL;
+    }
+}
+
+struct map_node * uncle(struct map_node * node){
+    struct map_node * g = grandparent(node);
+    
+    if(g == NULL){
+        return NULL;
+    } else if(node -> parent == g -> left){
+        return g -> right;
+    } else {
+        return g -> left;
+    }
 }
 
 int map_get(struct map_node * root, char * key){
     if(root -> key == NULL){
-        return NULL;
+        return ERROR;
     }
     
     if(strcmp(root -> key, key) == 0){
@@ -80,14 +158,14 @@ int map_get(struct map_node * root, char * key){
 }
 
 int op_char_to_int(char * op_code){
-    return SUCCESS;
+    return map_get(op_codes_tree, op_code);
 }
 
 int op_to_type(int op_code){
     int op_type[18] = {TYPE_NA, TYPE_R, TYPE_I, TYPE_R, TYPE_I, TYPE_R, TYPE_I, TYPE_I, TYPE_I, TYPE_I, TYPE_I, TYPE_I, TYPE_I, TYPE_I, TYPE_I, TYPE_J, TYPE_R, TYPE_J };
     return op_type[op_code];
 }
-=======
+
 //❤        L S  .  .  . f  r  o  m         h   e  r   e       ❤
 char *outputPath;
 
@@ -117,7 +195,33 @@ struct command{
 
 
 int main(int argc, char *argv[]){
-	if(argc!=2){
+    struct map_node codes_tree;
+    op_codes_tree = &codes_tree;
+    
+    op_codes_tree -> key = NULL;
+    op_codes_tree -> left = NULL;
+    op_codes_tree -> right = NULL; 
+
+    map_put(op_codes_tree, "halt", 0);
+    map_put(op_codes_tree, "add", 1);
+    map_put(op_codes_tree, "addi", 2);
+    map_put(op_codes_tree, "sub", 3);
+    map_put(op_codes_tree, "subi", 4);
+    map_put(op_codes_tree, "mul", 5);
+    map_put(op_codes_tree, "muli", 6);
+    map_put(op_codes_tree, "lw", 7);
+    map_put(op_codes_tree, "sw", 8);
+    map_put(op_codes_tree, "beq", 9);
+    map_put(op_codes_tree, "bne", 10);
+    map_put(op_codes_tree, "blt", 11);
+    map_put(op_codes_tree, "bgt", 12);
+    map_put(op_codes_tree, "ble", 13);
+    map_put(op_codes_tree, "bge", 14);
+    map_put(op_codes_tree, "jmp", 15);
+    map_put(op_codes_tree, "jr", 16);
+    map_put(op_codes_tree, "jal", 17);
+
+	/*if(argc!=2){
 		printf("usage: %s filename", argv[0]);
 	}
 	else{
@@ -137,16 +241,7 @@ int main(int argc, char *argv[]){
 
 			}
 
-	}
-    
-    //Testing tree
-    struct map_node map;
-    
-    map.left = NULL;
-    map.right = NULL;
-
-    map_put(&map, "pies", 4);
-    printf("%d\n", map_get(&map, "pies"));
+	}*/
     
     return 0;
 }
