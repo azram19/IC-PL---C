3b827c98a036f5f959579ece8f11628ec945fea0
Lukasz Koprowski <azram19@gmail.com>
2011-06-06 17:03:49 +0100

    Evaluating functions work.

--- IMPSA/assemble.c
+++ IMPSA/assemble.c
@@ -217,7 +217,7 @@ struct command {
  */
 struct command readToken() {
 	struct command *token;
-	token = (struct command *)malloc(1*sizeof(struct command));
+	token = (struct command *)malloc(sizeof(struct command));
 
 	char *rest;
 	int registersNumber;
@@ -466,6 +466,117 @@ int betole(int b){
     
     return l;
 }
+
+int op_prec(const char op){
+    if(op == '*') return 3;
+    if(op == '/') return 3;
+    if(op == '+') return 2;
+    if(op == '-') return 2;
+    if(op == '%') return 3;
+}
+
+char * evaluate_expression(char * expr){
+    char * output = malloc(32 * sizeof(char));
+    char stack[16];
+    char number[16];
+    int number_end = 0;
+    int stack_end = 0;
+    int output_end = 0;
+    int i = 0, j;
+    
+    memset(number, '\0', 16);
+    
+    while(expr[i] != '\0'){
+        if(isdigit(expr[i])){
+            number[number_end++] = expr[i];
+        } else if(isspace(expr[i])){
+            if(number_end == 0) {i++; continue;}
+            
+            //printf("d%s ", number);
+            for(j = 0; j < number_end; j++){
+                output[output_end++] = number[j];
+            }
+            output[output_end++] = ' ';
+            
+            number_end = 0;
+            memset(number, '\0', 16);
+            
+        } else if(expr[i] == '(') {
+            stack[stack_end++] = '(';
+        } else if(expr[i] == ')') {
+            int j = stack_end;
+            while(stack_end - 1 != '('){
+                output[output_end++] = stack[--stack_end];
+                output[output_end++] = ' ';
+            }
+            stack_end--;
+        } else {
+            int pre = op_prec(expr[i]);
+            while(op_prec(stack[stack_end - 1]) >= pre){
+                output[output_end++] = stack[--stack_end];
+                output[output_end++] = ' ';
+            }
+            stack[stack_end++] = expr[i];
+        }
+        i++;
+    }
+    
+    for(j = 0; j < number_end; j++){
+        output[output_end++] = number[j];
+    }
+    output[output_end++] = ' ';
+    
+    while(stack_end > 0){
+        output[output_end++] = stack[--stack_end];
+        output[output_end++] = ' ';
+    }
+    
+    return output;
+}
+
+int compute_RPN(char * expr){
+    int stack[16];
+    char number[16];
+    int number_end = 0;
+    int stack_end = 0;
+    int i = 0, a, b;
+    
+    memset(number, '\0', 16);
+    
+    while(expr[i] != '\0'){
+        if(isdigit(expr[i])){
+            number[number_end++] = expr[i];
+        } else if(isspace(expr[i])){
+            if(number_end == 0){i++; continue;}
+            stack[stack_end++] = atoi(number);
+            number_end = 0;
+            memset(number, '\0', 16);
+        } else {
+            a = stack[--stack_end];
+            b = stack[--stack_end];
+            
+           
+            if(expr[i] == '+'){
+                stack[stack_end++] = b + a;
+            } else if(expr[i] == '-'){
+                stack[stack_end++] = b - a;
+            } else if(expr[i] == '/'){
+                stack[stack_end++] = b / a;
+            } else if(expr[i] == '*'){
+                stack[stack_end++] = b * a;
+            } else if(expr[i] == '%'){
+                stack[stack_end++] = b % a;
+            }
+
+        }
+        i++;
+    }
+    
+    free(expr);
+    return stack[0];   
+}
+
+
 
 int main(int argc, char *argv[]) {
 	op_codes_tree = (struct map_node * ) malloc(sizeof(struct map_node));
